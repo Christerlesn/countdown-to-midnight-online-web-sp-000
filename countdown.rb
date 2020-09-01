cb0d369a9e57204b8cbe220787c47564693f9800
Christerle Nounoute <christerlesn@gmail.com>
2020-09-01 18:05:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,18 @@
-def countdown_with_sleep
- sleep 5
-end
 
 def countdown(countdown_output)
   while countdown_output > 0
     puts "#{countdown_output} SECOND(S)!"
-    countdown_with_sleep
     countdown_output -= 1
   end
     return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(countdown_output)
+ while countdown_output > 0
+   puts "#{countdown_output} SECOND(S)!"
+   countdown_output -= 1
+   sleep 1
+ end
+   return "HAPPY NEW YEAR!"
 end
